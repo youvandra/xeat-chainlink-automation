419acd2a9b0a71f774aea8d4b64d31d0de10280b
LeonardTarigan <leontrg32@gmail.com>
2023-11-23 19:45:41 +0700

    adjusting layouts for mobile view

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,8 @@ import MainLayout from "pages/layout"
 function HomePage() {
   return (
     <MainLayout>
-      <div className="flex h-screen flex-col bg-gradient-to-r from-xeat-black to-xeat-dark-blue">
-        <section className="flex w-full justify-end gap-5 p-10 pb-0">
+      <div className="flex h-screen basis-[80%] flex-col bg-gradient-to-r from-xeat-black to-xeat-dark-blue">
+        <section className="flex w-full flex-wrap justify-end gap-5 p-5 pb-0 md:p-10">
           <div className="rounded-xl bg-xeat-dark-grey px-7 py-1">0x34...8f8a</div>
           <div className="rounded-xl bg-xeat-teal px-7 py-1">50 ETH</div>
         </section>
@@ -15,14 +15,14 @@ function HomePage() {
             <Image src={"/assets/images/homepage-x-image.png"} alt={"x image"} fill />
           </div>
 
-          <div className="text-5xl">
+          <div className="text-2xl md:text-5xl">
             <h1 className="font-bold tracking-widest text-xeat-light-blue">XEAT</h1>
             <p>Save your tickets as an NFT collections.</p>
           </div>
-          <p className="text-xl font-bold">
+          <p className="text-sm font-bold md:text-xl">
             Elevate Your Experience, Secure Your Memories <br /> Transforming Tickets into Dynamic NFTs.
           </p>
-          <button className="flex items-center gap-2 rounded-lg bg-xeat-light-blue px-10 py-3">
+          <button className="flex items-center gap-2 rounded-lg bg-xeat-light-blue px-10 py-3 text-sm md:text-base">
             <PlayIcon className="fill-white" /> Explore NFT Tickets
           </button>
         </section>
--- pages/mycollections/index.tsx
+++ pages/mycollections/index.tsx
@@ -6,15 +6,15 @@ function MyCollectionsPage() {
   return (
     <MainLayout>
       <div className="flex h-screen flex-col bg-gradient-to-r from-xeat-black to-xeat-dark-blue">
-        <section className="flex w-full items-center justify-between gap-5 p-10">
+        <section className="flex w-full flex-wrap items-center justify-between gap-5 p-5 md:p-10">
           <h2 className="text-2xl font-semibold uppercase">NFT Collections</h2>
           <div className="flex gap-5">
             <div className="rounded-xl bg-xeat-dark-grey px-7 py-1">0x34...8f8a</div>
             <div className="rounded-xl bg-xeat-teal px-7 py-1">50 ETH</div>
           </div>
         </section>
-        <section className="flex flex-col gap-5 px-10">
-          <div className="flex w-2/5 items-center gap-0 overflow-hidden rounded-full border-1.5 border-xeat-dark-grey pl-4 pr-2">
+        <section className="flex flex-col gap-5 p-5 md:p-10">
+          <div className="flex w-full items-center gap-0 overflow-hidden rounded-full border-1.5 border-xeat-dark-grey pl-4 pr-2 md:w-2/5">
             <SearchIcon className="h-5 w-5 fill-white" />
             <input
               type="text"
@@ -23,7 +23,7 @@ function MyCollectionsPage() {
             />
           </div>
           <h2 className="text-xl font-medium">All Collections</h2>
-          <div className="grid grid-cols-5 gap-4">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
             {[...Array(10)].map((item, index) => {
               return <CollectionCard key={index} />
             })}
